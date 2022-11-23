eb15b8112a47dd1e6661c6aec47ba0c5ed518a62
Komiljon <komilriksiyev@gmail.com>
2022-11-23 15:04:02 +0500

    addUpButton

--- script.js
+++ script.js
@@ -41,3 +41,24 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbyzdghoqvJZ9x7ovrSw5I
       })
       .catch(error => console.error('Error!', error.message))
   })
+
+  // ------------------toTopButton------------------
+
+  let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
